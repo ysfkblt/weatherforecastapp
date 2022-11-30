2c622bb8156ec2dc7a1ad712cb792b4e2ef602f1
Kevin Lam <mtn013209@gmail.com>
2022-11-30 15:07:49 -0800

    changing to react

--- src/FrostDateCheck.js
+++ src/FrostDateCheck.js
@@ -1,101 +1,132 @@
-const userZoneNumber = 1 // ! PLACEHOLDER
+const FrostDatecheck = () => {
+  const userZoneNumber = 9 // ! PLACEHOLDER
 
-// initialize
-let firstFrostDate = null
-let lastFrostDate = null
-let daysToFirstFrost = null
-let daysToLastFrost = null
-let weeksToFirstFrost = null
-let weeksToLastFrost = null
+  // initialize
+  let firstFrostDate = null
+  let lastFrostDate = null
+  let daysToFirstFrost = null
+  let daysToLastFrost = null
+  let weeksToFirstFrost = null
+  let weeksToLastFrost = null
 
-const MS_IN_A_DAY = 24 * 60 * 60 * 1000 //hours * minutes * seconds * milliseconds
-
-// todo ? get more specific data for EX 9a,9b
-/** ! frost data. make changes here to change frost dates
- *  zone# : [last frost date, first frost date]
- *  aka zone#: [start of growing season, end of growing season]
- */
-const frostZoneMap = {
-  1: ["5/29", "8/29"],
-  2: ["5/19", "9/5"],
-  3: ["5/9", "9/12"],
-  4: ["5/4", "9/30"],
-  5: ["4/19", "10/18"],
-  6: ["4/12", "10/25"],
-  7: ["3/29", "11/7"],
-  8: ["3/21", "11/18"],
-  9: ["2/18", "12/5"],
-  10: ["null", "null"],
-  11: ["null", "null"],
-  12: ["null", "null"],
-  13: ["null", "null"],
-}
-
-function getFrostDates(zoneNum, frostZoneMap) {
-  if (frostZoneMap[zoneNum]) {
-    console.log(
-      "user zone and last and first frosts >>> ",
-      zoneNum,
-      ":",
-      frostZoneMap[zoneNum]
-    )
-    firstFrostDate = frostZoneMap[zoneNum][1]
-    lastFrostDate = frostZoneMap[zoneNum][0]
-  } else {
-    // TBD add error handling
-    console.log("ERROR: entered zone does not exist within dataset")
+  let plantTimingObject = {
+    zone: null,
+    firstFrost: null,
+    lastFrost: null,
+    currentDate: null,
+    frostSeason: null,
+    daysToFirstFrost: null,
+    daysToLastFrost: null,
+    weeksToFirstFrost: null,
+    weeksToLastFrost: null,
   }
-}
 
-// Checks if today's date is within the growing season
-// return true if OKAY to plant, return false if NOT GOOD TO PLANT (aka in frost zone time)
-function frostDateCheck(lastFrostDate, firstFrostDate) {
-  const dateStr = new Date().toLocaleDateString()
-  console.log("today's date: ", dateStr)
-  const [month1, day1, year1] = dateStr.split("/")
-  const date = new Date(+year1, month1 - 1, +day1)
+  const MS_IN_A_DAY = 24 * 60 * 60 * 1000 //hours * minutes * seconds * milliseconds
 
-  const startStr = firstFrostDate //EX dec 30 = "12/30"
-  const [month2, day2] = startStr.split("/")
-  const ffDate = new Date(+year1, month2 - 1, +day2)
-
-  const endStr = lastFrostDate // EX aug 24 = "08/24"
-  const [month3, day3] = endStr.split("/")
-  const lfDate = new Date(+year1, month3 - 1, +day3)
-
-  // handling zones 10,11,12,13 which currently have no frost dates.
-
-  if (lastFrostDate === "null" && firstFrostDate === "null") {
-    console.log("🌱 Your hardiness zone does not have typical frost 🌱")
-    // ! what to suggest for this? same as
-
-    // return TRUE // okay to plant
-  } else if (date > lfDate && date < ffDate) {
-    // in the growing season, search for how many weeks until FIRST frost
-    console.log(`🌱 today is in the growing season for your zone🌱`)
-    // console.log({ date }, { lfDate }, { ffDate })
-
-    daysToFirstFrost = (ffDate - date) / MS_IN_A_DAY
-    weeksToFirstFrost = Math.floor(daysToFirstFrost / 7) //rounding down...?
-
-    console.log(
-      `days to first frost: ${daysToFirstFrost} ; weeks to first frost: ${weeksToFirstFrost}`
-    )
-
-    // return daysToFirstFrost
-    // return weeksToFirstFrost
-    // return TRUE // okay to plant
-  } else {
-    // in the winter season, search for how many weeks until LAST frost
-    console.log(`🧊 today is in the frost season for your zone🧊`)
-    console.log({ date }, { lfDate }, { ffDate })
-    // return FALSE // not okay to plant
+  // todo ? get more specific data for EX 9a,9b
+  /** ! frost data. make changes here to change frost dates
+   *  zone# : [last frost date, first frost date]
+   *  aka zone#: [start of growing season, end of growing season]
+   */
+  const frostZoneMap = {
+    1: ["5/29", "8/29"],
+    2: ["5/19", "9/5"],
+    3: ["5/9", "9/12"],
+    4: ["5/4", "9/30"],
+    5: ["4/19", "10/18"],
+    6: ["4/12", "10/25"],
+    7: ["3/29", "11/7"],
+    8: ["3/21", "11/18"],
+    9: ["2/18", "12/5"],
+    10: ["null", "null"],
+    11: ["null", "null"],
+    12: ["null", "null"],
+    13: ["null", "null"],
   }
+
+  function getFrostDates(zoneNum, frostZoneMap) {
+    if (frostZoneMap[zoneNum]) {
+      console.log(
+        "user zone and last and first frosts >>> ",
+        zoneNum,
+        ":",
+        frostZoneMap[zoneNum]
+      )
+      firstFrostDate = frostZoneMap[zoneNum][1]
+      lastFrostDate = frostZoneMap[zoneNum][0]
+    } else {
+      // TBD add error handling
+      console.log("ERROR: entered zone does not exist within dataset")
+    }
+  }
+
+  // Checks if today's date is within the growing season
+  // return true if OKAY to plant, return false if NOT GOOD TO PLANT (aka in frost zone time)
+  function frostDateCheck(lastFrostDate, firstFrostDate) {
+    const dateStr = new Date().toLocaleDateString()
+    console.log("today's date: ", dateStr)
+    plantTimingObject.currentDate = dateStr
+    const [month1, day1, year1] = dateStr.split("/")
+    const date = new Date(+year1, month1 - 1, +day1)
+
+    const startStr = firstFrostDate //EX dec 30 = "12/30"
+    const [month2, day2] = startStr.split("/")
+    const ffDate = new Date(+year1, month2 - 1, +day2)
+
+    const endStr = lastFrostDate // EX aug 24 = "08/24"
+    const [month3, day3] = endStr.split("/")
+    const lfDate = new Date(+year1, month3 - 1, +day3)
+    const lfDateNextYear = new Date(+year1 + 1, month3 - 1, +day3)
+
+    // handling zones 10,11,12,13 which currently have no frost dates.
+    if (lastFrostDate === "null" && firstFrostDate === "null") {
+      console.log("🌱 Your hardiness zone does not have typical frost 🌱")
+      // ! what to suggest for this? same as
+      plantTimingObject.frostSeason = false
+      // return TRUE // okay to plant
+    } else if (date > lfDate && date < ffDate) {
+      console.log(`🌱 today is in the growing season for your zone🌱`)
+
+      daysToFirstFrost = Math.round((ffDate - date) / MS_IN_A_DAY)
+      weeksToFirstFrost = Math.round(daysToFirstFrost / 7) //rounding to nearest INT
+
+      console.log(
+        `days to first frost: ${daysToFirstFrost} ; weeks to first frost: ${weeksToFirstFrost}`
+      )
+
+      plantTimingObject.frostSeason = false
+      plantTimingObject.daysToFirstFrost = daysToFirstFrost
+      plantTimingObject.weeksToFirstFrost = weeksToFirstFrost
+    } else {
+      console.log(`🧊 today is in the frost season for your zone🧊`)
+      daysToLastFrost = Math.round((lfDateNextYear - date) / MS_IN_A_DAY)
+      weeksToLastFrost = Math.round(daysToLastFrost / 7) //rounding to nearest INT
+      console.log(
+        `days to last frost: ${daysToLastFrost} ; weeks to last frost: ${weeksToLastFrost}`
+      )
+
+      plantTimingObject.frostSeason = true
+      plantTimingObject.daysToLastFrost = daysToLastFrost
+      plantTimingObject.weeksToLastFrost = weeksToLastFrost
+    }
+  }
+
+  getFrostDates(userZoneNumber, frostZoneMap)
+  frostDateCheck(lastFrostDate, firstFrostDate)
+  // frostDateCheck("1/10", "11/20") //debug testing dates
+
+  plantTimingObject.zone = userZoneNumber
+  plantTimingObject.firstFrost = firstFrostDate
+  plantTimingObject.lastFrost = lastFrostDate
+
+  console.log(plantTimingObject)
+  // module.export = { plantTimingObject }
+
+  return (
+    <>
+      <div></div>
+    </>
+  )
 }
 
-getFrostDates(userZoneNumber, frostZoneMap)
-// frostDateCheck(lastFrostDate, firstFrostDate)
-frostDateCheck("11/15", "12/29") //debug testing dates
-
-// todo season checker?
-// todo what exactly we want to return?
+export default FrostDatecheck
---
+++ src/zzzzTest.js
@@ -0,0 +1,6 @@
+import plantTimingObject from "./FrostDateCheck.js"
+
+// const dateObject = require("./FrostDateCheck")
+
+// console.log(dateObject)
+console.log(plantTimingObject)
