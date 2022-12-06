5f18d5564d6e59bb762327315bc0af86780ced4f
Kevin Lam <mtn013209@gmail.com>
2022-12-06 10:16:21 -0800

    dude it should be working now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
 import {
-	SearchIcon,
-	ArrowDownIcon,
-	ArrowUpIcon,
-} from '@heroicons/react/outline';
-import background, { gradient } from '../components/background';
-import { shuffle } from 'lodash';
-const Home = () => {
-  
+  SearchIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+} from "@heroicons/react/outline"
+import background, { gradient } from "../components/background"
+import { shuffle } from "lodash"
+import UpdateZipCode from "../components/LocationUpdate"
+import PlantSuggestions from "../components/PlantSuggestions"
+import ToggleDark from "../components/toggleDark"
+import { ThemeContext, themes } from "../components/themeContext"
+
+const Home = (props) => {
+  const [search, setSearch] = useState("")
+  const [info, setInfo] = useState({})
+  const [grad, setgrad] = useState(null)
+  const [zone, setZone] = useState("")
+  const [zip, setZip] = useState("")
+  const [userId, setUserId] = useState("")
+  const [darkMode, setDarkMode] = useState(true)
 
   async function getData() {
     await fetch(
@@ -23,9 +34,9 @@ const Home = () => {
             current: d.current.temp_c,
             max: d.forecast.forecastday[0].day.maxtemp_c,
             min: d.forecast.forecastday[0].day.mintemp_c,
-          }
+          },
         })
-      );
+      )
   }
 
   // ZONE API
@@ -36,7 +47,6 @@ const Home = () => {
     return data
   }
 
-
   // const wormIdCollection = collection(db, "worms")
   // const q = query(wormIdCollection, where("id", "==", props.userId))
   // const wormCollection = collection(db, "worms", currentChild, "journal")
@@ -45,9 +55,7 @@ const Home = () => {
     async function getZone() {
       let zoneResults = await fetchZone(search)
       {
-        zoneResults ? (
-          setZone(zoneResults)
-        ) : setZone("")
+        zoneResults ? setZone(zoneResults) : setZone("")
       }
     }
     getZone()
@@ -67,58 +75,57 @@ const Home = () => {
   // SEARCH
 
   function handleButtonClick() {
-    getData();
+    getData()
   }
 
   function handleKeyPress(e) {
-    if (e.key === 'Enter') handleButtonClick();
+    if (e.key === "Enter") handleButtonClick()
   }
 
   function handleSearch(e) {
-    setSearch(e.target.value);
-    setZip(e.target.value);
-    handleButtonClick();
+    setSearch(e.target.value)
+    setZip(e.target.value)
+    handleButtonClick()
   }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   useEffect(() => {
-    setgrad(shuffle(gradient).pop());
-  }, []);
+    setgrad(shuffle(gradient).pop())
+  }, [])
 
   return (
-// Dynamic Background
-  <div
-			style={
-				info.condition?.toLowerCase() === 'clear'
-					? { backgroundImage: background.clear }
-					: info.condition?.toLowerCase() === 'sunny'
-					? { backgroundImage: background.sunny }
-					: info.condition?.toLowerCase().includes('cloudy')
-					? { backgroundImage: background.cloudy }
-					: info.condition?.toLowerCase().includes('rain') ||
-					  info.condition?.toLowerCase().includes('drizzle')
-					? { backgroundImage: background.rainy }
-					: info.condition?.toLowerCase().includes('snow') ||
-					  info.condition?.toLowerCase().includes('sleet')
-					? { backgroundImage: background.snow }
-					: info.condition?.toLowerCase().includes('overcast')
-					? { backgroundImage: background.overcast }
-					: { backgroundImage: grad }
-			}
-			className='flex flex-row  text-black items-center justify-center h-screen bg-center bg-cover select-none'
-		>
-
+    // Dynamic Background
+    <div
+      style={
+        info.condition?.toLowerCase() === "clear"
+          ? { backgroundImage: background.clear }
+          : info.condition?.toLowerCase() === "sunny"
+          ? { backgroundImage: background.sunny }
+          : info.condition?.toLowerCase().includes("cloudy")
+          ? { backgroundImage: background.cloudy }
+          : info.condition?.toLowerCase().includes("rain") ||
+            info.condition?.toLowerCase().includes("drizzle")
+          ? { backgroundImage: background.rainy }
+          : info.condition?.toLowerCase().includes("snow") ||
+            info.condition?.toLowerCase().includes("sleet")
+          ? { backgroundImage: background.snow }
+          : info.condition?.toLowerCase().includes("overcast")
+          ? { backgroundImage: background.overcast }
+          : { backgroundImage: grad }
+      }
+      className="flex flex-row  text-black items-center justify-center h-screen bg-center bg-cover select-none"
+    >
       {/* DARKMODE */}
       <header className="header-container">
         <ThemeContext.Consumer>
           {({ changeTheme }) => (
             <ToggleDark
               toggleDark={() => {
-                setDarkMode(!darkMode);
-                changeTheme(darkMode ? themes.light : themes.dark);
+                setDarkMode(!darkMode)
+                changeTheme(darkMode ? themes.light : themes.dark)
               }}
             />
           )}
@@ -126,7 +133,7 @@ const Home = () => {
       </header>
 
       {/* Update user */}
-      {userId && zip.length === 5 && zone ?
+      {userId && zip.length === 5 && zone ? (
         <>
           <UpdateZipCode
             userId={userId}
@@ -135,83 +142,75 @@ const Home = () => {
             coordinates={zone.coordinates}
           />
         </>
-        : null
-      }
+      ) : null}
 
       {/* Search Bar */}
-      <div className='flex flex-row h-16 sm:h-24   absolute'>
-				<input
-					className='bg-transparent placeholder:text-black text-lg focus:outline-none border-transparent focus:border-transparent focus:ring-0 sm:text-xl font-light self-end mb-1 mr-10'
-					type='text'
-					spellCheck='false'
-					value={search}
-					placeholder='please enter location'
-					onChange={handleSearch}
-					onFocus={(e) => (e.target.placeholder = '')}
-					onBlur={(e) =>
-						(e.target.placeholder = 'please enter location')
-					}
-					onKeyPress={handleKeyPress}
-				/>
+      <div className="flex flex-row h-16 sm:h-24   absolute">
+        <input
+          className="bg-transparent placeholder:text-black text-lg focus:outline-none border-transparent focus:border-transparent focus:ring-0 sm:text-xl font-light self-end mb-1 mr-10"
+          type="text"
+          spellCheck="false"
+          value={search}
+          placeholder="please enter location"
+          onChange={handleSearch}
+          onFocus={(e) => (e.target.placeholder = "")}
+          onBlur={(e) => (e.target.placeholder = "please enter location")}
+          onKeyPress={handleKeyPress}
+        />
 
-				<div className='self-end mb-1  '>
-					<SearchIcon
-						className='cursor-pointer h-6 sm:h-7 opacity-70'
-						onClick={handleButtonClick}
-					/>
-				</div>
-			</div>
+        <div className="self-end mb-1  ">
+          <SearchIcon
+            className="cursor-pointer h-6 sm:h-7 opacity-70"
+            onClick={handleButtonClick}
+          />
+        </div>
+      </div>
 
       {/* Hidden info display, reveals after search button click */}
-      <div className='grid overflow-hidden grid-cols-2 grid-rows-2 gap-10 sm:gap-40 sm:mt-72 mt-56 sm:mr-0 mr-4'>
-				<div className='row-span-2 justify-self-end'>
-					{info.temp ? (
-						<p className='text-end sm:text-9xl text-7xl font-light tracking-tighter'>
-							{info.temp?.current}
-							<span className=' align-top  text-lg sm:font-light font-normal sm:text-3xl'>
-								°
-							</span>
-						</p>
-					) : null}
-				</div>
-				<div className='row-span-2  sm:mt-3 mt-2  justify-self-start truncate'>
-					<p className=' text-start sm:text-3xl font-light sm:pb-1 sm:ml-1'>
-						{info.condition}
-					</p>
-					{info.temp ? (
-						<p className='sm:text-2xl  text-start font-light'>
-							<ArrowUpIcon className='sm:h-4  h-2 inline-flex align-middle' />
-							{info.temp?.max}
-							<span className='align-top font-normal sm:text-base text-xs'>
-								°
-							</span>{' '}
-							<ArrowDownIcon className='sm:h-4 h-2 inline-flex align-middle' />
-							{info.temp?.min}
-							<span className='align-top font-normal sm:text-base text-xs'>
-								°
-							</span>
-						</p>
-					) : null}
-
-					<p className='sm:text-xl text-xs  text-start font-light  whitespace-nowrap  sm:mt-1 sm:ml-1'>
-						{info.country}
-					</p>
-          {zone.zone ? (
-            <p className=''>
-              Hardiness Zone {zone.zone}
+      <div className="grid overflow-hidden grid-cols-2 grid-rows-2 gap-10 sm:gap-40 sm:mt-72 mt-56 sm:mr-0 mr-4">
+        <div className="row-span-2 justify-self-end">
+          {info.temp ? (
+            <p className="text-end sm:text-9xl text-7xl font-light tracking-tighter">
+              {info.temp?.current}
+              <span className=" align-top  text-lg sm:font-light font-normal sm:text-3xl">
+                °
+              </span>
             </p>
           ) : null}
         </div>
+        <div className="row-span-2  sm:mt-3 mt-2  justify-self-start truncate">
+          <p className=" text-start sm:text-3xl font-light sm:pb-1 sm:ml-1">
+            {info.condition}
+          </p>
+          {info.temp ? (
+            <p className="sm:text-2xl  text-start font-light">
+              <ArrowUpIcon className="sm:h-4  h-2 inline-flex align-middle" />
+              {info.temp?.max}
+              <span className="align-top font-normal sm:text-base text-xs">
+                °
+              </span>{" "}
+              <ArrowDownIcon className="sm:h-4 h-2 inline-flex align-middle" />
+              {info.temp?.min}
+              <span className="align-top font-normal sm:text-base text-xs">
+                °
+              </span>
+            </p>
+          ) : null}
 
+          <p className="sm:text-xl text-xs  text-start font-light  whitespace-nowrap  sm:mt-1 sm:ml-1">
+            {info.country}
+          </p>
+          {zone.zone ? <p className="">Hardiness Zone {zone.zone}</p> : null}
+        </div>
 
-        {props.userId ?
-
-          <PlantSuggestions userId={props.userId} /> :
+        {props.userId ? (
+          <PlantSuggestions userId={props.userId} />
+        ) : (
           <PlantSuggestions userId={"NA"} />
-        }
-
+        )}
       </div>
-
     </div>
-  );
+  )
 }
+
+export default Home
