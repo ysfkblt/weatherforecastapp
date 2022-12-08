f2b6ebdf3a89b85a9407aa189a9d15d6ece4a68b
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2022-12-08 17:52:12 -0500

    simple bug in router

--- src/pages/AppRoutes.js
+++ src/pages/AppRoutes.js
@@ -7,12 +7,9 @@ import Journal from "./Journal"
 import AllPlantsView from "./allPlants/AllPlantsView"
 import SinglePlantView from "./SinglePlantView"
 import Auth from "../components/Auth"
-<<<<<<< HEAD
 import User from "./User"
 
-=======
 import Favorites from "./Favorites"
->>>>>>> 827ba518973c315df2a9e9112e23b577782a11d5
 
 const AppRoutes = () => {
   const [user, setUser] = useState("")
@@ -27,14 +24,11 @@ const AppRoutes = () => {
     <>
       <Routes>
         <Route path="/signUp" element={<Auth />} />
-<<<<<<< HEAD
         <Route path="/development" element={<AllPlantsView />} />
         <Route path="/development/:plantId" element={<SinglePlantView />} />
         <Route path="/user" element={<User />} />
-=======
         <Route path="/development" element={<AllPlantsView userId={user.uid} />} />
         <Route path="/development/:plantId" element={<SinglePlantView userId={user.uid} />} />
->>>>>>> 827ba518973c315df2a9e9112e23b577782a11d5
         {user ? (
           <>
             <Route path="/" element={<App userId={user.uid} />} />
